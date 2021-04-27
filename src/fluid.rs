dbbb33935a0cdbe507ef72beeae0c991cdc4e20e
Oresavna <vanessa.rojas@protonmail.com>
2021-04-27 11:17:15 +0200

    Apply 3D but only can see a slice

--- src/fluid.rs
+++ src/fluid.rs
@@ -1,38 +1,55 @@
-//#![allow(dead_code)]
+#![allow(dead_code)]
 //use std::mem;
 pub const TEX_SIZE: usize = 256;
+pub const X_SIZE: usize = 256;
+pub const Y_SIZE: usize = 256;
+pub const Z_SIZE: usize = 2;
+pub const SIZE: [usize; 3] = [X_SIZE, Y_SIZE, Z_SIZE];
+
 #[derive(Clone, Copy, Default)]
 pub struct Vel {
     pub x: f32,
     pub y: f32,
+    pub z: f32,
 }
 pub struct Pos {
     pub x: usize,
     pub y: usize,
+    pub z: usize,
 }
 pub struct Grid<'a, const M: usize> {
     pub x_vel: &'a mut [f32; M],
     pub y_vel: &'a mut [f32; M],
+    pub z_vel: &'a mut [f32; M],
 }
 impl<'a, const M: usize> Grid<'a, M> {
-    pub fn new(vx_grid: &'a mut [f32; M], vy_grid: &'a mut [f32; M]) -> Self {
+    pub fn new(vx_grid: &'a mut [f32; M], vy_grid: &'a mut [f32; M], vz_grid: &'a mut [f32; M]) -> Self {
         // Velocity grids
         Self {
             x_vel: vx_grid,
             y_vel: vy_grid,
+            z_vel: vz_grid,
         }
     }
-    // pub fn get(&self, pos: Pos) -> Vel{
-    //     let x = self.x_vel[pos.x + pos.y*TEX_SIZE];
-    //     let y = self.y_vel[pos.x + pos.y*TEX_SIZE];
-    //     Vel {x,y}
-    // }
-    pub fn set(&mut self, pos: Pos, vel: Vel) {
-        self.x_vel[pos.x + pos.y * TEX_SIZE] = vel.x;
-        self.y_vel[pos.x + pos.y * TEX_SIZE] = vel.y;
+    pub fn get_index(&self, pos: Pos)-> usize {
+        return pos.x + X_SIZE*(pos.y+ Y_SIZE * pos.z);
     }
-    pub fn arrays(&'a self) -> (&'a [f32; M], &'a [f32; M]) {
-        (&self.x_vel, &self.y_vel)
+
+    pub fn get(&self, pos: Pos) -> Vel{
+        let index = self.get_index(pos);
+        let x = self.x_vel[index];
+        let y = self.y_vel[index];
+        let z = self.y_vel[index];
+        Vel {x,y,z}
+    }
+    pub fn set(&mut self, pos: Pos, vel: Vel) {
+        let index = self.get_index(pos);
+        self.x_vel[index] = vel.x;
+        self.y_vel[index] = vel.y;
+        self.z_vel[index] = vel.z;
+    }
+    pub fn arrays(&'a self) -> (&'a [f32; M], &'a [f32; M], &'a [f32; M]) {
+        (&self.x_vel, &self.y_vel, &self.z_vel)
     }
 }
 
@@ -371,9 +388,8 @@ fn step_vel<const N: usize>(
 // }
 
 pub fn step_fluid<const N: usize>(
-    dens_grid: &mut [f32; N],
+    _dens_grid: &mut [f32; N],
     grid: &mut Grid<N>,
-    grid_w: u32,
     dt: f32,
     diff: f32,
     borders: bool,
@@ -384,5 +400,5 @@ pub fn step_fluid<const N: usize>(
     // Step density, alter density grid
 
     //step_dens(dens_grid, grid.x_vel, grid.y_vel, grid_w, dt, diff, borders);
-    step_vel(grid.x_vel, grid.y_vel, grid_w, dt, diff, borders);
+    step_vel(grid.x_vel, grid.y_vel, TEX_SIZE as u32, dt, diff, borders);
 }
--- src/main.rs
+++ src/main.rs
@@ -5,37 +5,37 @@ mod fluid;
 
 use fluid::*;
 use glium::backend::glutin_backend::GlutinFacade;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 #[derive(Copy, Clone)]
 struct Vertex {
-    pos: [f32; 2],
-    uv: [f32; 2],
+    pos: [f32; 3],
+    uv: [f32; 3],
 }
 implement_vertex!(Vertex, pos, uv);
 
 static VERT_SRC: &'static str = r#"
     #version 140
 
-    in vec2 pos;
-    in vec2 uv;
+    in vec3 pos;
+    in vec3 uv;
 
-    out vec2 v_uv;
+    out vec3 v_uv;
 
     void main() {
       v_uv = uv;
-      gl_Position = vec4(pos, 0.0, 1.0);
+      gl_Position = vec4(pos, 1.0);
     }
 "#;
 
 static FRAG_SRC: &'static str = r#"
     #version 140
 
-    uniform highp sampler2D tex;
-    uniform highp sampler2D tex1;
-    uniform highp sampler2D tex2;
+    uniform highp sampler3D tex;
+    uniform highp sampler3D tex1;
+    uniform highp sampler3D tex2;
 
-    in vec2 v_uv;
+    in vec3 v_uv;
 
     out vec4 color;
 
@@ -56,33 +56,54 @@ fn setup_shader(display: &GlutinFacade) -> glium::Program {
 fn main() {
     let display = setup_display();
     let shader = setup_shader(&display);
-    let (display_w, display_h) = display.get_window().unwrap().get_inner_size().unwrap();
+    let (w_size, h_size) = display.get_window().unwrap().get_inner_size().unwrap();
 
     let vbo_data = vec![
         Vertex {
-            pos: [-1.0, -1.0],
-            uv: [0.0, 0.0],
+            pos: [-1.0, 1.0, -1.0],
+            uv: [0.0, 1.0, 0.0],
         },
         Vertex {
-            pos: [1.0, -1.0],
-            uv: [1.0, 0.0],
+            pos: [1.0, 1.0, -1.0],
+            uv: [1.0, 1.0, 0.0],
         },
         Vertex {
-            pos: [1.0, 1.0],
-            uv: [1.0, 1.0],
+            pos: [1.0, -1.0, 1.0],
+            uv: [1.0, 0.0, 1.0],
+        },
+        // Vertex {
+        //     pos: [-1.0, -1.0, -1.0],
+        //     uv: [0.0, 0.0, 0.0],
+        // },
+        //
+        // Vertex {
+        //     pos: [1.0, -1.0, -1.0],
+        //     uv: [1.0, 0.0, 0.0],
+        // },
+        // Vertex {
+        //     pos: [1.0, 1.0, -1.0],
+        //     uv: [1.0, 1.0, 0.0],
+        // },
+        Vertex {
+            pos: [-1.0, 1.0, -1.0],
+            uv: [0.0, 1.0, 0.0],
         },
         Vertex {
-            pos: [-1.0, -1.0],
-            uv: [0.0, 0.0],
+            pos: [-1.0, -1.0, 1.0],
+            uv: [0.0, 0.0, 1.0],
         },
         Vertex {
-            pos: [-1.0, 1.0],
-            uv: [0.0, 1.0],
+            pos: [1.0, -1.0, 1.0],
+            uv: [1.0, 0.0, 1.0],
         },
-        Vertex {
-            pos: [1.0, 1.0],
-            uv: [1.0, 1.0],
-        },
+        // Vertex {
+        //     pos: [1.0, 1.0, 1.0],
+        //     uv: [1.0, 1.0, 1.0],
+        // },
+        // Vertex {
+        //     pos: [-1.0, 1.0, 1.0],
+        //     uv: [0.0, 1.0, 1.0],
+        // },
     ];
 
     let vbo = glium::VertexBuffer::new(&display, &vbo_data).unwrap();
@@ -91,20 +112,16 @@ fn main() {
     // Set up fluids
 
     // Velocity grids
-    let mut vx_grid_ = [0_f32; TEX_SIZE * TEX_SIZE];
-    let mut vy_grid_ = [0_f32; TEX_SIZE * TEX_SIZE];
-    let mut tex_data_ = [0.0; TEX_SIZE * TEX_SIZE];
+    let mut vx_grid_ = [0_f32; X_SIZE * Y_SIZE * Z_SIZE];
+    let mut vy_grid_ = [0_f32; X_SIZE * Y_SIZE * Z_SIZE];
+    let mut vz_grid_ = [0_f32; X_SIZE * Y_SIZE * Z_SIZE];
+    let mut tex_data_ = [0.0; X_SIZE * Y_SIZE * Z_SIZE];
     // Create texture data buffer for fluid
 
-    let (mut prev_mx, mut prev_my) = (0, 0);
-    let mut grid = Grid::new(&mut vx_grid_, &mut vy_grid_);
-    // 4 tuple, ABXY, AB for position and XY for velocity. Every frame the
-    //   velocity cell this correcponds to gets set to XY.
+    let mut grid = Grid::new(&mut vx_grid_, &mut vy_grid_, &mut vz_grid_);
 
-    // let now = Instant::now();
     let mut last_t = Instant::now();
-    // let max_nanoseconds = u64::MAX / 1_000_000_000;
-    // let mut duration = Duration::new(max_nanoseconds, 0);
+
     let mut fps_list = Vec::<u128>::new();
     // let mut t = Duration::default();
     loop {
@@ -124,63 +141,69 @@ fn main() {
         for ev in display.poll_events() {
             match ev {
                 glium::glutin::Event::Closed => return, // the window has been closed by the user
-                // glium::glutin::Event::MouseMoved(x, y) => {
-                //     let pos = Pos {
-                //         x: ((x as f32 / display_w as f32) * TEX_SIZE as f32) as usize,
-                //         y: (((display_h as f32 - y as f32) / display_h as f32) * TEX_SIZE as f32)
-                //             as usize,
-                //     };
-                //
-                //     //println!("{}, {}", x, y);
-                //     let ix = pos.x + pos.y * TEX_SIZE;
-                //     if ix >= tex_data_.len() {
-                //         continue;
-                //     }
-                //
-                //     tex_data_[pos.x + pos.y * TEX_SIZE] = 1.0;
-                //
-                //     let mut vel = Vel::default();
-                //     if prev_mx != 0 && prev_my != 0 {
-                //         vel.x = (pos.x as f32 - prev_mx as f32) * 2000.0;
-                //         vel.y = (pos.y as f32 - prev_my as f32) * 2000.0;
-                //     }
-                //     prev_mx = pos.x;
-                //     prev_my = pos.y;
-                //
-                //     grid.set(pos, vel);
-                // }
                 _ => (),
             }
         }
 
-        grid.set(Pos { x: 100, y: 100 }, Vel { x: 10.0, y: 10.0 });
+        // Adding point velocity sources to the grid
+
+        grid.set(
+            Pos {
+                x: 200,
+                y: 100,
+                z: 1,
+            },
+            Vel {
+                x: 100.0,
+                y: -100.0,
+                z: 0.0,
+            },
+        );
+        grid.set(
+            Pos {
+                x: 100,
+                y: 250,
+                z: 1,
+            },
+            Vel {
+                x: 0.0,
+                y: -200.0,
+                z: 0.0,
+            },
+        );
+
+
+
 
         // Process fluids
-        fluid::step_fluid(&mut tex_data_, &mut grid, TEX_SIZE as u32, ms as f32/1000.0, 0.1, true);
+        fluid::step_fluid(&mut tex_data_, &mut grid, ms as f32 / 1000.0, 0.1, true);
 
         // Re buffer texture
         use std::borrow::Cow;
-        let raw_tex_2d = glium::texture::RawImage2d {
+        let raw_tex_3d = glium::texture::RawImage3d {
             data: Cow::from(tex_data_[..].to_vec()),
-            width: TEX_SIZE as u32,
-            height: TEX_SIZE as u32,
+            width: X_SIZE as u32,
+            height: Y_SIZE as u32,
+            depth: Z_SIZE as u32,
             format: glium::texture::ClientFormat::F32,
         };
-        let raw_tex_2d1 = glium::texture::RawImage2d {
+        let raw_tex_3d1 = glium::texture::RawImage3d {
             data: Cow::from(grid.x_vel[..].to_vec()),
-            width: TEX_SIZE as u32,
-            height: TEX_SIZE as u32,
+            width: X_SIZE as u32,
+            height: Y_SIZE as u32,
+            depth: Z_SIZE as u32,
             format: glium::texture::ClientFormat::F32,
         };
-        let raw_tex_2d2 = glium::texture::RawImage2d {
+        let raw_tex_3d2 = glium::texture::RawImage3d {
             data: Cow::from(grid.y_vel[..].to_vec()),
-            width: TEX_SIZE as u32,
-            height: TEX_SIZE as u32,
+            width: X_SIZE as u32,
+            height: Y_SIZE as u32,
+            depth: Z_SIZE as u32,
             format: glium::texture::ClientFormat::F32,
         };
-        let texture = glium::texture::Texture2d::new(&display, raw_tex_2d).unwrap();
-        let texture1 = glium::texture::Texture2d::new(&display, raw_tex_2d1).unwrap();
-        let texture2 = glium::texture::Texture2d::new(&display, raw_tex_2d2).unwrap();
+        let texture = glium::texture::Texture3d::new(&display, raw_tex_3d).unwrap();
+        let texture1 = glium::texture::Texture3d::new(&display, raw_tex_3d1).unwrap();
+        let texture2 = glium::texture::Texture3d::new(&display, raw_tex_3d2).unwrap();
         // Load texture into uniforms
         let uniforms = uniform! {
           tex: texture.sampled()
